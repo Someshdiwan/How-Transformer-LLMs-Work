1f750b3630effa1e2fcdf136ca5cbe45984e7585
https://github.com/Someshdiwan <someshdiwan369@gmail.com>
2025-12-02 11:27:02 +0530

    fix streamlite

--- tokenizer/app/main_app.py
+++ tokenizer/app/main_app.py
@@ -3,7 +3,9 @@
 import os
 import json
 from typing import List, Dict, Any, Optional
-
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 import torch
 import streamlit as st
 import pandas as pd
